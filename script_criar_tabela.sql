4ce9378b60f8adbc88600feab126ca1637bf437d
Dan5326 <127957242+D53261@users.noreply.github.com>
2024-06-12 20:48:53 -0300

    script_criar_tabela.sql

---
+++ script_criar_tabela.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `db_turma_202400207`.`tbl_alunos` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `nome` VARCHAR(255) NULL,
+    `idade` VARCHAR(255) NULL,
+    `escolaridade` VARCHAR(255) NULL,
+    `trabalha_ou_nao` VARCHAR(255) NULL,
+    PRIMARY KEY(`id`));
