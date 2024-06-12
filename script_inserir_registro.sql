8ba3b40714a3923e0e6467dc991090570b1be802
Dan5326 <127957242+D53261@users.noreply.github.com>
2024-06-12 20:57:22 -0300

    script_inserir_registro.sql

---
+++ script_inserir_registro.sql
@@ -0,0 +1,11 @@
+INSERT INTO `db_turma_202400207`.`tbl_alunos` (
+    `nome`,
+    `idade`,
+    `escolaridade`,
+    `trabalha_ou_nao`
+) VALUES (
+    'Cremildo',
+    '29',
+    'Ensino médio completo',
+    'Não trabalha'
+)
